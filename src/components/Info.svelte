5257b6a9a69e4b4698b7db9985cf02b9d71ca3a8
Hallef Santos <hallefsantos@gmail.com>
2020-10-14 15:27:17 -0300

    genesis

---
+++ src/components/Info.svelte
@@ -0,0 +1,12 @@
+<script>
+
+   export let label = '';
+
+</script>
+
+<div class="mb-3">
+   <span>{label}:</span>
+   <div class="text-2xl text-green-400">
+      <slot></slot>
+   </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Roboto Condensed', 'sans-serif']
+    },
+  },
+  variants: {},
+  plugins: []
+}
